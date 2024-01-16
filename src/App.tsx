caa38a080abb0d73b31c54e41e539a7d318b5969
Sai Teja <saiteja.g1801@gmail.com>
2024-01-16 18:07:20 +0530

    implemented expand rows in mui datagrid

--- src/App.tsx
+++ src/App.tsx
@@ -8,95 +8,37 @@ import { styled } from "@mui/material/styles";
 import { IconButton } from "@mui/material";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import { useEffect, useState } from "react";
+import { createRoot } from "react-dom/client";
 
 const CustomGrid = styled(DataGrid)<DataGridProps>(() => ({
   // "& .MuiDataGrid-row": {
   //   minHeight: "100px !important",
   //   maxHeight: "100px !important",
   // },
-  // "& .Mui-selected": {
-  //   minHeight: "100px !important",
-  //   maxHeight: "100px !important",
-  // },
+  "& .MuiDataGrid-virtualScrollerContent": {
+    // height: "auto !important",
+  },
 }));
-
-// Function to create a new table
-function ExpandedTable(data, columns) {
-  return ReactDOM.createRoot(document.getElementById("expanded-row")!).render(
-    <React.StrictMode>
-      <CustomGrid
-        disableVirtualization
-        rows={data}
-        columns={columns}
-        disableRowSelectionOnClick
-      />
-    </React.StrictMode>
-  );
-}
-
-function createNewTable() {
-  let table = document.createElement("table");
-  table.innerHTML = `
-      <tr>
-          <th>Column 1</th>
-          <th>Column 2</th>
-      </tr>
-      <tr>
-          <td>Data 1</td>
-          <td>Data 2</td>
-      </tr>
-  `;
-  return table;
-}
-
-const NewTable = ({ data }) => {
-  // Render your table using the passed data
-  return (
-    <table>
-      <tr>
-        <td>test</td>
-        <td>test</td>
-        <td>test</td>
-        <td>test</td>
-        <td>test</td>
-        <td>test</td>
-        <td>test</td>
-      </tr>
-    </table>
-  );
-};
 
 function injectTableAfterSelectedRow(id: number) {
   const selectedRow = document.querySelector(`[data-id="${id}"]`);
   if (selectedRow) {
-    debugger;
     // Create a container element for the portal
     const container = document.createElement("div");
     container.id = `table-container-${id}`;
 
     // Insert the container into the DOM
     selectedRow?.parentNode?.insertBefore(container, selectedRow.nextSibling);
-    const Tab = <NewTable data={[]} />;
-
-    // Use createPortal to render the JSX component into the container
-    ReactDOM.createPortal(<>as</>, container);
   } else {
     console.log("No selected row found");
   }
-
-  // if (selectedRow) {
-  //   const newTable = createNewTable();
-  //   selectedRow?.parentNode?.insertBefore(newTable, selectedRow.nextSibling);
-  // } else {
-  //   console.log("No selected row found");
-  // }
 }
 
 export default function App() {
   const [clickedIndex, setClickedIndex] = useState(-1);
-  const [expandedRowData, setExpandedRowData] = useState();
+  // eslint-disable-next-line
+  const [expandedRowData, setExpandedRowData] = useState<any>({});
   const columns = [
     {
       field: "id",
@@ -105,12 +47,27 @@ export default function App() {
           <IconButton
             onClick={() => {
               console.log(cellValues);
+              if (clickedIndex) {
+                const domNode = document.getElementById(
+                  `table-container-${clickedIndex}`
+                );
+                if (domNode && domNode.parentNode) {
+                  domNode.parentNode.removeChild(domNode);
+                }
+              }
               if (clickedIndex === cellValues.value) {
+                setExpandedRowData({});
                 setClickedIndex(-1);
+                const domNode = document.getElementById(
+                  `table-container-${cellValues.value}`
+                );
+                if (domNode && domNode.parentNode) {
+                  domNode.parentNode.removeChild(domNode);
+                }
               } else {
                 setClickedIndex(cellValues.value);
-                setExpandedRowData(cellValues.row);
                 injectTableAfterSelectedRow(cellValues.value);
+                setExpandedRowData(cellValues.row);
               }
             }}
           >
@@ -132,10 +89,58 @@ export default function App() {
     { field: "last_name", headerName: "Last name", flex: 1 },
     { field: "ph_no", headerName: "Phone number", flex: 1 },
   ];
+  useEffect(() => {
+    const subcolumns = [
+      {
+        field: "id",
+        renderCell: () => {
+          return null;
+        },
+        width: 60,
+      },
+      {
+        field: "first_name",
+        headerName: "First name",
+        flex: 1,
+      },
+      { field: "last_name", headerName: "Last name", flex: 1 },
+      { field: "ph_no", headerName: "Phone number", flex: 1 },
+    ];
+
+    if (expandedRowData.family) {
+      const table = (
+        <div style={{ width: "496px" }}>
+          <div style={{ width: "100%" }}>
+            <CustomGrid
+              sx={{
+                "& .MuiDataGrid-root": {
+                  borderWidth: "0px !important",
+                },
+              }}
+              rows={expandedRowData?.family}
+              columns={subcolumns}
+              slots={{
+                columnHeaders: () => null,
+              }}
+              hideFooter
+            />{" "}
+          </div>
+        </div>
+      );
+
+      const domNode = document.getElementById(
+        `table-container-${expandedRowData.id}`
+      );
+      if (domNode) {
+        const root = createRoot(domNode);
+        root.render(table);
+      }
+    }
+  }, [expandedRowData]);
 
   return (
     <div style={{ width: "500px", marginLeft: "4rem" }}>
-      <div style={{ height: 350, width: "100%" }}>
+      <div style={{ height: "auto", width: "100%" }}>
         <CustomGrid
           disableVirtualization
           rows={data}
@@ -144,25 +149,10 @@ export default function App() {
             if (params.id === clickedIndex) return "expand-row";
             return "";
           }}
-          // disableRowSelectionOnClick
+          disableRowSelectionOnClick
+          autoHeight
         />
       </div>
     </div>
   );
 }
-
-// Function to inject the table after the selected row
-
-// Call the function to perform the action
-
-// https://mui.com/x/react-data-grid/style/#styling-rows
-
-// if (selectedRow) {
-//   const newTable = ExpandedTable(expandedRowData, columns);
-//   if (selectedRow.parentNode && ref?.current) {
-//     // selectedRow.parentNode.insertBefore(newTable, selectedRow.nextSibling);
-//     ref?.current?.insertAdjacentHTML("beforeend", `<div id="box></div>`);
-//   }
-// } else {
-//   console.log("No selected row found");
-// }
